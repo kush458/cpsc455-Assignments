95f5b33b4300cb98bb8fe745501ebac67ab93f6e
kushar77 <kusharora339@gmail.com>
2023-05-16 20:36:10 -0700

    added search

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const inventory = document.getElementsByClassName('inventory')[0];
+
 const renderInitData = () => {
+
     fetch('/initData.json')
         .then(resp => resp.json())
         .then(data => {
-            const inventory = document.getElementsByClassName('inventory')[0];
 
             data.forEach(item => {
                 const card = document.createElement('div');
@@ -14,7 +16,7 @@ const renderInitData = () => {
                         <div class="nameAndPrice">
                             <div class="topInfo">
                                 <p>Item Name</p>
-                                <p>${item.name}</p>
+                                <p class="itemName">${item.name}</p>
                             </div>
                             <div class="topInfo">
                                 <p>Price</p>
@@ -35,5 +37,66 @@ const renderInitData = () => {
             console.log('Error: ', error);
         });
 }
+
+const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    const itemName = document.getElementById('itemName').value;
+    const itemPrice = document.getElementById('itemPrice').value;
+    const itemImg = document.getElementById('itemImg').value;
+    const itemDesc = document.getElementById('itemDesc').value;
+
+    const card = document.createElement('div');
+    card.className = 'itemCard';
+
+    card.innerHTML = `
+        <img src="${itemImg}" alt="${itemName}"/>
+        <div class="infoContainer">
+            <div class="nameAndPrice">
+                <div class="topInfo">
+                    <p>Item Name</p>
+                    <p class="itemName">${itemName}</p>
+                </div>
+                <div class="topInfo">
+                    <p>Price</p>
+                    <p>$${itemPrice}</p>
+                </div>
+            </div>
+            <div class="topInfo">
+                <p>Description</p>
+                <p>${itemDesc}</p>
+            </div>
+        </div>
+    `;
+
+    inventory.appendChild(card);
+    
+    itemForm.reset();
+}
+
+const search = (event) => {
+    const itemCards = document.getElementsByClassName('itemCard');
+    const searchTerm = event.target.value.toLowerCase();
+
+    for (const itemCard of itemCards) {
+        const itemName = itemCard.getElementsByClassName('itemName')[0].textContent.toLowerCase();
+
+        if (itemName.includes(searchTerm)) {
+            itemCard.style.display = 'flex';
+        } else {
+            itemCard.style.display = 'none';
+        }
+    }
+}
+
+const itemForm = document.getElementById('itemForm');
+const clearAllBtn = document.getElementById('clearAllButton');
+const searchInput = document.getElementById('searchInput');
+
+itemForm.addEventListener('submit', handleOnSubmit);
+searchInput.addEventListener('input', search);
+clearAllBtn.addEventListener('click', () => {
+    itemForm.reset();
+});
 
 renderInitData();
